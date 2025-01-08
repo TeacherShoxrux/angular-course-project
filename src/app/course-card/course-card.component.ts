9a4263bce628f314d0457d2530fdb32363663c06
ShoxruxTeacher <yarashovshokhrukh@gmail.com>
2025-01-08 22:50:17 +0500

    switch add and style class changed NG

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,20 +1,33 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component,NgModule, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Course } from '../model/course';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'course-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css'
 })
 export class CourseCardComponent implements OnInit {
+cardClass(): any {
+ if(this.course.category == 'BEGINNER' )return{ 'beginner': this.course.category == 'BEGINNER' };
+}
+cardStyles(): any {
+return {'text-decoration':'underline'}; 
+}
 courseViewed() {
   this.courseSelected.emit(this.course);
 }
-  @Input()
+  @Input() 
   course:Course;
+
+  @Input({required:true})
+  index:number;
+
   @Output('courseSelected')
   courseSelected = new EventEmitter<Course>();
+
   constructor() { 
 
   }
